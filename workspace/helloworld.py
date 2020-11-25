a7fec94438ad70805e8fef751e24a6b0579d3b8c
sauravbsaha <sauravsaha@ymail.com>
2020-11-25 19:34:28 +0000

    Basic variable for printing

--- workspace/helloworld.py
+++ workspace/helloworld.py
@@ -1,2 +1,8 @@
 # This is my first Python Script #
+
+# Printing Hello World 
 print("Hello World! The world of python looks awesome.") 
+
+# Using variable for print statement
+language = "python"
+print("The world of",language, "is awesome")
