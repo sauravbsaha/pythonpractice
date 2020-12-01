f607df92d706268a59a5fe7158ea8e778f78610f
sauravbsaha <sauravsaha@ymail.com>
2020-12-01 20:02:41 +0000

    colon  as a scope definer in python

--- workspace/function.py
+++ workspace/function.py
@@ -1,6 +1,6 @@
 # This is example for python functions
 # Defining function
-def printingFunction():
+def printingFunction(): #: is a scope definer
     print("I am an example of a python function")
 
 printingFunction()
