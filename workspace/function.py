5373290ce43d952f86df9286c3e68a81d6872067
sauravbsaha <sauravsaha@ymail.com>
2020-12-01 20:17:51 +0000

    Supply of arguments in python functions

--- workspace/function.py
+++ workspace/function.py
@@ -4,3 +4,15 @@ def printingFunction(): #: is a scope definer
     print("I am an example of a python function")
 
 printingFunction()
+
+# Taking arguments in functions
+def addFunction(x,y):
+    print("The addition of the two number is:", x+y)
+
+addFunction(2,4)
+
+#  Providing default parameters in functions
+def addFunctionWDefaultArguments(number1=10, number2=12): #order of arguments are not important
+    print("The addition of the two number is:", number1+number2)
+
+addFunctionWDefaultArguments(2,3)
