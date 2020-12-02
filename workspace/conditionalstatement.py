b9c768193284093aaba37fd263a12bc4b60a2f18
sauravbsaha <sauravsaha@ymail.com>
2020-12-02 21:12:37 +0000

    conditional statements

--- workspace/conditionalstatement.py
+++ workspace/conditionalstatement.py
@@ -3,6 +3,7 @@
 def comparisonOperator(number1, number2):
     if(number2 > number1):
         print("Second number is greater than First number")
+    #elif can also be used as else if statements
     else:
         print("First number is larger than second one")
 
