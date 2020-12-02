d3be49142299682840b02648c93ec36475ee3623
sauravbsaha <sauravsaha@ymail.com>
2020-12-02 19:44:10 +0000

    functions

--- workspace/function.py
+++ workspace/function.py
@@ -1,18 +1,19 @@
-# This is example for python functions
-# Defining function
-def printingFunction(): #: is a scope definer
-    print("I am an example of a python function")
+# # This is example for python functions
+# # Defining function
+# def printingFunction(): #: is a scope definer
+#     print("I am an example of a python function")
 
-printingFunction()
+# printingFunction()
 
-# Taking arguments in functions
-def addFunction(x,y):
-    print("The addition of the two number is:", x+y)
+# # Taking arguments in functions
+# def addFunction(x,y):
+#     print("The addition of the two number is:", x+y)
 
-addFunction(2,4)
+# addFunction(2,4)
 
 #  Providing default parameters in functions
 def addFunctionWDefaultArguments(number1=10, number2=12): #order of arguments are not important
     print("The addition of the two number is:", number1+number2)
+    
+addFunctionWDefaultArguments(2,3) 
 
-addFunctionWDefaultArguments(2,3)
