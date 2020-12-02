21948342a661b881699c45e1f6a1b3403d268b07
sauravbsaha <sauravsaha@ymail.com>
2020-12-02 21:11:28 +0000

    conditional operators

---
+++ workspace/conditionalstatement.py
@@ -0,0 +1,12 @@
+# Conditional statements
+
+def comparisonOperator(number1, number2):
+    if(number2 > number1):
+        print("Second number is greater than First number")
+    else:
+        print("First number is larger than second one")
+
+
+
+if __name__=="__main__":
+    comparisonOperator(13,4)
