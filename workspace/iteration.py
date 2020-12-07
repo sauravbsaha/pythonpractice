5a049e190b0a1be0c463fab978ececaac907cb56
sauravbsaha <sauravsaha@ymail.com>
2020-12-07 21:03:58 +0000

    FirstEDit

---
+++ workspace/iteration.py
@@ -0,0 +1,16 @@
+# Loops in Python
+
+# Iteration using for loop
+def forloop(number):
+    for i in range(1,number +1):
+        print(i)
+
+forloop(15)
+
+# Iteration using while loop
+def whileloop(number):
+    i=0
+    while(i<number+1):
+        print(i)
+        i=i+1
+whileloop(100)
